1a95a021891a97f8bf8e4d35c510a3dd609efcdf
Seyedarman Fatemi <seyedarman.fatemi@urbansportsclub.com>
2023-09-27 11:16:11 +0200

    Add godotenv for reading environment variables from .env file

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// load .env file - the name is optional
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT is not found in th environment")
+	}
+
+	fmt.Println("A:" + portString)
 }
